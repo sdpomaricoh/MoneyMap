bcebbae18fd95291bf6585317a284f5f1e10cf50
Sergio Pomárico <sergiodavid21@gmail.com>
2018-05-27 11:30:15 -0500

    google maps integration

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,14 +1,108 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions, Marker, LatLng, MarkerOptions } from '@ionic-native/google-maps';
+import { GeolocationService } from '../../services/geolocation.service';
+import { db } from '../../database';
+
 
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html'
 })
 export class MapPage {
-  title: string;
-  constructor(public navCtrl: NavController) {
-    this.title = "Sobre nosotros";
+
+  title: String;
+  map: GoogleMap;
+  coords: any;
+  transactions: any;
+
+  constructor(public navCtrl: NavController, public geolocator: GeolocationService, public toastCtrl: ToastController) {
+    this.title = "Mapa";
+    this.transactions = [];
+  }
+
+  ionViewWillEnter() {
+    this.loadMap();
+  }
+
+  loadMap(){
+
+    this.geolocator.get().then((position) => {
+
+      this.coords = position.coords;
+
+      let mapOptions: GoogleMapOptions = {
+        controls: {
+          'compass': true,
+          'myLocationButton': true,
+          'myLocation': true,
+          'indoorPicker': true,
+          'zoom': true,
+        },
+        gestures: {
+          scroll: true,
+          tilt: true,
+          zoom: true,
+          rotate: true
+        },
+        preferences: {
+          padding: {
+            left: 10,
+            top: 10,
+            bottom: 10,
+            right: 10
+          }
+        },
+        camera: {
+          target: {
+            lat: this.coords.latitude,
+            lng: this.coords.longitude
+          },
+          zoom: 18,
+          tilt: 30
+        }
+      };
+
+      this.map = GoogleMaps.create('map', mapOptions);
+
+      this.map.on(GoogleMapsEvent.MAP_READY).subscribe(()=>{
+        this.loadMarkers()
+      })
+
+    }).catch((err)=>{
+      let toast = this.toastCtrl.create({
+        message: 'No se puede acceder a la geolocalización',
+        duration: 3000
+      });
+      toast.present();
+    })
+
+  }
+
+  loadMarkers(){
+    db.getAll().then((results)=>{
+      this.transactions = results;
+      this.loadTransactionMarkers(this.transactions)
+    })
+  }
+
+  loadTransactionMarkers(transactions){
+    this.transactions.forEach(transaction => {
+
+      let position: LatLng = new LatLng(transaction.lat, transaction.lng);
+
+      let options: MarkerOptions = {
+        position: position,
+        title: transaction.title
+      }
+
+      this.map.addMarker(options).then((marker: Marker) => {
+        marker.showInfoWindow();
+      });
+
+    });
+
   }
 
 }
